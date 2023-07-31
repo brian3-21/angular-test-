7d901b62bcd3275289cc17033ebc1f3ee8cad141
Briannn <brianperezdota@gmail.com>
2023-07-31 13:41:50 -0400

    create components

--- src/app/tareas/components/list-tareas/list-tareas.component.ts
+++ src/app/tareas/components/list-tareas/list-tareas.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Tarea } from '../../interfaces/tarea.interface';
 
 @Component({
   selector: 'app-list-tareas',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./list-tareas.component.css']
 })
 export class ListTareasComponent {
+
+  public listaT:Tarea[]=[
+    {
+      nombre:'Una tarea',
+      descripcion: 'Tarea'
+    },
+    {
+      nombre:'Una tarea',
+      descripcion: 'Tarea'
+    },
+    {
+      nombre:'Una tarea',
+      descripcion: 'Tarea'
+    },
+    {
+      nombre:'Una tarea',
+      descripcion: 'Tarea'
+    }
+  ];
+
+  eliminarTarea(index:number){
+    this.listaT.splice(index,1)
+  }
 
 }
